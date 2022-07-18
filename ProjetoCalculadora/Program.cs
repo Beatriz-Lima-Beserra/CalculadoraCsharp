b213698ce505a515a5915f5d9bdd7d8f71020415
Beatriz-Lima-Beserra <beatrizlimaa37@gmail.com>
2022-07-18 10:31:14 -0300

    Primeira calculadora em CSharp

---
+++ ProjetoCalculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjetoCalculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando;
+
+            Console.Write("Digite o multiplicando: ");
+            multiplicando = Convert.ToInt32(Console.ReadLine());
+
+            for (int multiplicador = 0; multiplicador <= 10; multiplicador ++)
+            {
+                Console.WriteLine(string.Format("\t\t\t{0}\t*\t{1}\t=\t{2}", multiplicando, multiplicador, multiplicando * multiplicador));
+            }
+            Console.WriteLine("Aperte alguma tecla!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
